490969dd55238c8a70bd3363657d120937529545
bmoxey <bmoxey@gmail.com>
2023-08-10 12:29:48 +1000

    Initial Commit

---
+++ MHSOBHC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MHSOBHC
+//
+//  Created by Brett Moxey on 10/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MHSOBHC/MHSOBHCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHSOBHCApp.swift
+//  MHSOBHC
+//
+//  Created by Brett Moxey on 10/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct MHSOBHCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
